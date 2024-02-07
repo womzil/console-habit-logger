d10a5aa48fd41adbd1272f1dc0f4484a762f0ec0
womzil <28115365+womzil@users.noreply.github.com>
2024-02-07 16:02:17 +0100

    Added basic functions

--- HabitDatabase.cs
+++ HabitDatabase.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.Data.Sqlite;
 
 /*
+ *
  * All functions used to operate on the database are here.
  *
  * The database is stored in "habit-logger.sqlite" file by default.
  *
  * Every habit has its own table in it.
+ *
  */
 
 namespace habit_logger;
@@ -14,20 +16,60 @@ public static class HabitDatabase
 {
     static string _connectionString = "Data Source=habit-logger.sqlite;";
 
-    public static void Create()
+    public static void Create(string name, string unit)
     {
         SQLitePCL.Batteries.Init();
 
         using SqliteConnection connection = new SqliteConnection(_connectionString);
         connection.Open();
 
-        string createTableSql = "CREATE TABLE IF NOT EXISTS habits (Id INTEGER PRIMARY KEY, Name TEXT)";
-        using (SqliteCommand command = new SqliteCommand(createTableSql, connection))
+        // Create table of habits
+        List<string> queries = ["CREATE TABLE IF NOT EXISTS habits (Id INTEGER PRIMARY KEY, Name TEXT, Unit TEXT)"];
+
+        queries.Add($"INSERT INTO habits (Name, Unit) VALUES (\"{name}\", \"{unit}\")");
+
+        // Create habit table
+        queries.Add($"CREATE TABLE IF NOT EXISTS \"{name}\" (Id INTEGER PRIMARY KEY, Amount REAL, Time TEXT)");
+
+        for (int i = 0; i < queries.Count; i++)
         {
-            command.ExecuteNonQuery();
-            Console.WriteLine("Table created successfully.");
+            using (SqliteCommand command = new SqliteCommand(queries[i], connection))
+            {
+                command.ExecuteNonQuery();
+                Console.WriteLine(i == 0 ? "Table of habits created successfully." : $"Habit table \"{name}\" created successfully.");
+            }
         }
 
         connection.Close();
+    }
+
+    public static void InsertData(string name, double amount)
+    {
+        SQLitePCL.Batteries.Init();
+
+        using SqliteConnection connection = new SqliteConnection(_connectionString);
+        connection.Open();
+
+        // Create table of habits
+        string query = $"INSERT INTO \"{name}\" (Amount, Time) VALUES (\"{amount}\", datetime())";
+
+            using (SqliteCommand command = new SqliteCommand(query, connection))
+            {
+                command.ExecuteNonQuery();
+                Console.WriteLine($"Added new entry with value of \"{amount}\" to the habit \"{name}\" successfully.");
+            }
+
+        connection.Close();
+    }
+
+    // Just for testing purposes
+    public static void CreateSampleData()
+    {
+        Create("Water drunk", "litres (l)");
+        for (int i = 0; i < 1000; i++)
+        {
+            double randomNumber = new Random().NextDouble() * 5;
+            InsertData("Water drunk", randomNumber);
+        }
     }
 }
